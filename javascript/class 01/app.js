9f70344e71042f836c560e27dbd3868451274fdb
Tayyabah Alam <tayyabah441@gmail.com>
2026-01-10 19:36:42 +0500

    javascript all classes

---
+++ javascript/class 01/app.js
@@ -0,0 +1,48 @@
+console.log("javascript connected!");
+
+alert ("Hello Javascript");
+
+// primitives starts here:
+
+// String:
+var student="Tayyabah";
+console.log(student);
+
+// Number:
+var age=15;
+console.log(age);
+
+// Boolean:
+var isstudent=false;
+console.log(isstudent);
+
+// Undefined:
+var isfromschool;
+console.log(isfromschool);
+
+// Null:
+var fromhere=null;
+console.log(fromhere);
+
+
+// By mentioning datatype:
+
+// String:
+var student="Tayyabah";
+console.log(typeof student);
+
+// Number:
+var age=15;
+console.log(typeof age);
+
+// Boolean:
+var isstudent=false;
+console.log(typeof isstudent);
+
+// Undefined:
+var isfromschool;
+console.log(typeof isfromschool);
+
+// Null:
+var fromhere=null;
+console.log(typeof fromhere);
