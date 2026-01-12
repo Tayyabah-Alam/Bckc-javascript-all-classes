cccbfbc06314a586b7999d046f92a1ef245db3b4
Tayyabah Alam <tayyabah441@gmail.com>
2026-01-12 18:52:29 +0500

    add new file

---
+++ javascript/class 02/app.js
@@ -0,0 +1,47 @@
+console.log("javascript connected!")
+
+// Cases in programming:
+
+// Camel case:
+var studentName="Tayyabah";
+console.log(studentName);
+
+// Snake case:
+var student_name="Afraa";
+console.log(student_name)
+
+// Pascal case:
+var StudentName="Maida";
+console.log(StudentName)
+
+
+
+// Math Expression:
+
+// Familiar operator:
+var x=10;
+var y=20;
+console.log(x+y-y*x+x/y +y+x)
+
+
+// Unfamiliar operator:
+
+// num++ :
+var num=10;
+num++
+console.log(num)
+
+// num-- :
+var num=15;
+num--
+console.log(num)
+
+// Increament/Decreament:
+var z=4;
+var result=z++ + --z - ++z - ++z +z +z + --z -z - z-- + z++
+console.log(result)
+
+
+// Eliminating Ambiguity:
+var totalcost=(4*5)*4/2;
+console.log(totalcost)
