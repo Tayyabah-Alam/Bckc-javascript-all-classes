5be54432ccf6712f6b76655fc2af4724f392b975
Tayyabah Alam <tayyabah441@gmail.com>
2026-01-27 21:52:28 +0500

    class 03

---
+++ javascript/class 03/app.js
@@ -0,0 +1,67 @@
+console.log("Javascript connected!")
+
+// Concatination:
+var firstName ="Tayyabah";
+var secondName ="Alam"
+console.log(firstName+ " "+secondName)
+
+var firstName ="Tayyabah";
+var secondName ="Alam"
+alert(firstName+ " "+secondName)
+
+var a="2 plus 2 equals"
+console.log( a+ 2+2 )
+
+var a="2 plus 2 equals"
+console.log( a+(2+2) )
+
+
+// Prompt:
+var city=prompt("Type your city name");
+console.log("Your city name is" +" "+ city)
+
+
+// IF AND ELSE:
+var hania =prompt("Where does she live?")
+if(hania==="Islamabad"){
+    console.log("correct answer!");
+}else{
+    console.log("wrong answer!");
+}
+
+
+//  OPERATORS:
+//     =   (Assigning operator)
+//     = =   (Equality operator)
+//     = = =  (Stricktly equality operator)
+//     >  (Greater than operator)
+//     <  (less than operator)
+//     >=  (Greater than equal to operator)
+//     <=  (less than equal to operator)
+//     !=   (Not equal to operator)
+//     != =  (Stricktly not equal to operator)
+
+
+console.log(10==10)
+result=true
+
+// console.log(Hello===hello)
+// result=false
+
+console.log(10==="10")
+result=false
+
+console.log(10>20)
+result=false
+
+console.log(20<20)
+result=false
+
+console.log(20<=20)
+result=true
+
+console.log(10>=20)
+result=false
+
+console.log(10 !== "10");
+result=true
